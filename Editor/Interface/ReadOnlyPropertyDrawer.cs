f84d6079f06c4f19c4987ebac41e1dd97059d3ab
Burcklé Loïc <loic.burckle@epitech.eu>
2022-03-12 17:29:03 +0100

    test 2

---
+++ Editor/Interface/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEditor;
+using Xyliem.Interface;
+
+
+namespace Xyliem.Interface
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label);
+            GUI.enabled = true;
+        }
+    }
+}
--- Interface/ReadOnlyAttribute.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Xyliem.Interface
-{
-    public class ReadOnlyAttribute : PropertyAttribute
-    {
-        
-    }
-}
